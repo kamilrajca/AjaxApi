23b0726402c16beb0e795627ac17eddde2e4c6e3
Kamil <kamil.rajca@gmail.com>
2017-02-19 11:58:50 +0100

    create cw 11.5

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// // cwiczenie 11.4 za pomocą jQuery
+// var url = 'http://api.icndb.com/jokes/random';
+
+// var $button = $('#get-joke').click(function(){
+//   getJoke();
+// });
+
+// var $paragraph = $('#joke');
+
+// // implementacja funkcji getJoke()
+// function getJoke() {
+// $.ajax({
+//   method: 'GET',
+//   url: url, success:function(res) {
+//     $paragraph.text(res.value.joke);
+//   }
+// });
+// }
+
+// // automatyczne wyswietlenie bez koniecznosći klikania 
+// // przycisku po załadowaniu pliku
+// getJoke();
+
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
+
+function getQuote() {
+  $.getJSON(quoteUrl, createTweet);
+}
+function createTweet(input) {
+  if (!input.quoteAuthor.length) {
+    input.quoteAuthor = "Unknown author";
+  }
+  var tweetText = "Quote of the day - " + input.quoteText + " Author: " + input.quoteAuthor;
+
+  if (tweetText.length > 140) {
+    getQuote();
+  } else {
+  var tweet = tweetLink + encodeURIComponent(tweetText);
+    $('.quote').text(input.quoteText);
+    $('.author').text("Author: " + input.quoteAuthor);
+    $('.tweet').attr('href', tweet);
+  }
+}
+$(document).ready(function() {
+  getQuote();
+  $('.trigger').click(function() {
+    getQuote();
+  })
+});
