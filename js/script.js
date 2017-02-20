39b17ba8a5de939f0715ed3999ad4267e544d6df
Kamil <kamil.rajca@gmail.com>
2017-02-20 19:52:32 +0100

    poprawienie wciec w kodzie

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-var url = 'http://api.icndb.com/jokes/random';
+var url         = 'http://api.icndb.com/jokes/random';
+var button      = document.getElementById('get-joke');
+var paragraph   = document.getElementById('joke');
 
-var button = document.getElementById('get-joke');
+
 button.addEventListener('click', function(){
   getJoke();
 });
 
-var paragraph = document.getElementById('joke');
-
-var xhr = new XMLHttpRequest();
-
 // implementacja funkcji getJoke()
 function getJoke() {
+  var xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.addEventListener('load', function(){
     var response = JSON.parse(xhr.response);
@@ -22,5 +21,3 @@ function getJoke() {
 // automatyczne wyswietlenie bez koniecznosći klikania 
 // przycisku po załadowaniu pliku
 getJoke();
-
-
